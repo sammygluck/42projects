339aa20b03da1a38abecc05225bfe3ff99a8200d
Samuel Gluck <sgluck@fu-r2-p4.s19.be>
2023-02-02 13:20:05 +0100

    feb 2 13:20

--- ft_printf/test.c
+++ ft_printf/test.c
@@ -1,19 +1,49 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sgluck <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/29 11:34:31 by sgluck            #+#    #+#             */
-/*   Updated: 2023/01/29 11:39:21 by sgluck           ###   ########.fr       */
+/*   Created: 2023/01/15 16:40:45 by sgluck            #+#    #+#             */
+/*   Updated: 2023/02/02 11:38:59 by sgluck           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <unistd.h>
+
+int	ft_putnbr_fd(int n, int fd, int *retn)
+{
+	char	nbr;
+
+	if (n == -2147483648)
+	{
+		write(fd, "-2", 2);
+		n = 147483648;
+	}
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		*retn += 1;
+		n = -n;
+	}
+	if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd, retn);
+		n = n % 10;
+	}
+	nbr = n + '0';
+	write(fd, &nbr, 1);
+	*retn += 1;
+	return (*retn);
+}
 
 int	main(void)
-{	
-	char *str = "hello %c";
-	printf(str, 'c');
+{
+	int	retn;
+
+	retn = 0;
+	printf("\n%i", ft_putnbr_fd(-12345, 1, &retn));
+	return (0);
 }
